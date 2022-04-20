21d5324e51d1f314b26df503493d8d3adb7d5be5
Angel <santoyox714@gmail.com>
2022-04-19 19:54:07 -0700

    Update config.php

--- php-chatRoom/config.php
+++ php-chatRoom/config.php
@@ -5,10 +5,10 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'luna');
 define('DB_NAME', 'chatroom');
 
-/* Attempt to connect to MySQL database */
+/*  connect to database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-// Check connection
+// Check connect
 if($link === false){
     die("ERROR: Could not connect to database. " . mysqli_connect_error());
 }
