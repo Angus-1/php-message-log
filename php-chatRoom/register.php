949bb8b508701dee23dabd565e06a69cb110be69
Angel <santoyox714@gmail.com>
2022-04-19 19:57:39 -0700

    Update register.php

--- php-chatRoom/register.php
+++ php-chatRoom/register.php
@@ -1,31 +1,31 @@
 <?php
-// Include config file
+
 require_once "config.php";
 
-// Define variables and initialize with empty values
+// Define/initialize variables
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
 
-// Processing form data when form is submitted
+// Processing form data 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-    // Validate username
+    // Validate 
     if(empty(trim($_POST["username"]))){
         $username_err = "Please enter a username.";
     } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
         $username_err = "Username can only contain letters, numbers, and underscores.";
     } else{
-        // Prepare a select statement
+        // select statement
         $sql = "SELECT id FROM users WHERE username = ?";
 
         if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
+            // Bind variables to statement
             mysqli_stmt_bind_param($stmt, "s", $param_username);
 
             // Set parameters
             $param_username = trim($_POST["username"]);
 
-            // Attempt to execute the prepared statement
+            //execute statement
             if(mysqli_stmt_execute($stmt)){
                 /* store result */
                 mysqli_stmt_store_result($stmt);
@@ -63,14 +63,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
     }
 
-    // Check input errors before inserting in database
+    // Check input errors before insert
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
 
-        // Prepare an insert statement
+        // Prepare insert statement
         $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
 
         if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
+            // Bind variables to statement 
             mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
 
             // Set parameters
