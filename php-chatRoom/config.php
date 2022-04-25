d9561e741ef6603c72fdd885d413547b2f1cf6b5
Angel <santoyox714@gmail.com>
2022-04-25 13:52:28 -0700

    Update config.php

--- php-chatRoom/config.php
+++ php-chatRoom/config.php
@@ -4,6 +4,7 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'luna');
 define('DB_NAME', 'chatroom');
+//for home server 
 
 // connect to database 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
